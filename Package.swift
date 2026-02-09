ba0a7a59ff3759291afa581f0cd8f209ecc81add
jaeyeonee <jaeyeonee@t-win.kr>
2026-02-09 10:41:55 +0900

    fix: package name

--- Package.swift
+++ Package.swift
@@ -2,27 +2,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "CapacitorIosCustomWebView",
+    name: "JaeyeoneeCapacitorIosWebview",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "CapacitorIosCustomWebView",
-            targets: ["CapacitorIosCustomWebView"])
+            name: "JaeyeoneeCapacitorIosWebview",
+            targets: ["JaeyeoneeCapacitorIosWebview"])
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
     ],
     targets: [
         .target(
-            name: "CapacitorIosCustomWebView",
+            name: "JaeyeoneeCapacitorIosWebview",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm")
             ],
             path: "ios/Sources/CustomWebViewPlugin"),
         .testTarget(
-            name: "CapacitorIosCustomWebViewTests",
-            dependencies: ["CapacitorIosCustomWebView"],
+            name: "JaeyeoneeCapacitorIosWebviewTests",
+            dependencies: ["JaeyeoneeCapacitorIosWebview"],
             path: "ios/Tests/CustomWebViewPluginTests")
     ]
 )
